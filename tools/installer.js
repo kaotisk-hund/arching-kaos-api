12a7ae84d6dd44de030d74aa2c137cc30efd0578
kaotisk <kaotisk@arching-kaos.tk>
2020-04-22 20:12:50 +0300

    Added installer

---
+++ tools/installer.js
@@ -0,0 +1,20 @@
+/*
+ * Installer script for Arching Kaos API
+ */
+
+var fs = require('fs')
+
+// Get configuration
+var config = require('../config')
+
+// Create if not exists downloads folder
+if(!fs.existsSync(config.dowonloadsFolder)) {
+	fs.mkdirSync(config.downloadsFolder)
+}
+
+
+// Create if not exists settings directory
+if(!fs.existsSync(config.settingsDir)) {
+	fs.mkdirSync(config.settingsDir)
+}
+
