536590de46f4c6028cc794a7bb8e167bb9026c12
kaotisk <kaotisk@arching-kaos.tk>
2020-09-12 02:11:40 +0300

    Updated config so it can work anywhere

--- config.js
+++ config.js
@@ -2,10 +2,13 @@
  * Configuration
  */
 
+const home = require('os').homedir;
+
 module.exports = {
 	port: 3001,
-	ipfsGateway : 'http://127.0.0.1:8080/ipfs/',
-	downloadsFolder : '/home/kaotisk/projects/arching-kaos-api/downloads/',
-	ipList: '/home/kaotisk/projects/arching-kaos-api/ipList.json',
-	showsList: '/home/kaotisk/projects/arching-kaos-api/all.json'
+	ipfsGateway : 'https://ipfs.arching-kaos.tk/ipfs/',
+	esotericGateway : 'http://127.0.0.1:8080/ipfs/',
+	downloadsFolder : home+'/.arching-kaos-api/downloads',
+	ipList: home+'/.arching-kaos-api',
+	showsList: home+'/.arching-kaos-api/shows.json'
 }
