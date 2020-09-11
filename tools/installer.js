23c00cd26609249461d3c63ca3c1552d07353ffc
kaotisk <kaotisk@arching-kaos.tk>
2020-09-12 02:33:53 +0300

    Fix the installer

--- tools/installer.js
+++ tools/installer.js
@@ -7,14 +7,12 @@ var fs = require('fs')
 // Get configuration
 var config = require('../config')
 
-// Create if not exists downloads folder
-if(!fs.existsSync(config.dowonloadsFolder)) {
-	fs.mkdirSync(config.downloadsFolder)
-}
-
-
 // Create if not exists settings directory
 if(!fs.existsSync(config.settingsDir)) {
 	fs.mkdirSync(config.settingsDir)
 }
 
+// Create if not exists downloads folder
+if(!fs.existsSync(config.dowonloadsFolder)) {
+	fs.mkdirSync(config.downloadsFolder)
+}
