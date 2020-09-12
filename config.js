e97fccfe0108b3d5e36d60cc17f06bb28c5e830e
kaotisk <kaotisk@arching-kaos.tk>
2020-09-12 03:00:21 +0300

    added trailing slash at the end of the paths

--- config.js
+++ config.js
@@ -6,10 +6,10 @@ const home = require('os').homedir;
 
 module.exports = {
 	port: 3001,
-	settingsDir : home+'/.arching-kaos-api',
+	settingsDir : home+'/.arching-kaos-api/',
 	ipfsGateway : 'https://ipfs.arching-kaos.tk/ipfs/',
 	esotericGateway : 'http://127.0.0.1:8080/ipfs/',
-	downloadsFolder : home+'/.arching-kaos-api/downloads',
+	downloadsFolder : home+'/.arching-kaos-api/downloads/',
 	ipList: home+'/.arching-kaos-api/ipList.json',
 	showsList: home+'/.arching-kaos-api/shows.json'
 }
