0327a1cd5b3414289226c1c06cf2b810b0f7c4fc
kaotisk <kaotisk@arching-kaos.com>
2021-03-03 22:02:45 +0200

    Added Dockerfile and a script for creating .arching-kaos-api folder

---
+++ api-dir.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+export ARCHING_KAOS_API_DIR=$HOME/.arching-kaos-api
+mkdir -p $ARCHING_KAOS_API_DIR
+cd $ARCHING_KAOS_API_DIR
+echo "[{"ip":"127.0.0.1"}]" > ipList.json
+echo "[{}]" > shows.json
--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
 	port: 3001,
 	settingsDir : home+'/.arching-kaos-api/',
 	ipfsGateway : 'https://ipfs.arching-kaos.tk/ipfs/',
-	esotericGateway : 'http://127.0.0.1:8080/ipfs/',
+	esotericGateway : 'http://172.17.0.1:8080/ipfs/',
 	downloadsFolder : home+'/.arching-kaos-api/downloads/',
 	ipList: home+'/.arching-kaos-api/ipList.json',
 	showsList: home+'/.arching-kaos-api/shows.json'
