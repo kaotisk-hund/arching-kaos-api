98810352a38c3951a5947da0c2b4368bb0d4fd97
kaotisk <kaotisk@arching-kaos.com>
2021-03-04 17:52:36 +0200

    Fixed configuration

--- api-dir.sh
+++ api-dir.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 export ARCHING_KAOS_API_DIR=$PWD/storage/.arching-kaos-api
 mkdir -p $ARCHING_KAOS_API_DIR
-cd $ARCHING_KAOS_API_DIR
-echo "[{"ip":"127.0.0.1"}]" > ipList.json
-echo "[{}]" > shows.json
+cp ipList.json-sample $ARCHING_KAOS_API_DIR/ipList.json
+cp shows.json-sample $ARCHING_KAOS_API_DIR/shows.json
