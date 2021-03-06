b09dde32032e2c7bcef86a039e9260498528d2c3
kaotisk <kaotisk@arching-kaos.com>
2021-03-06 19:42:41 +0200

    Replace the {$IPFS_SERVER_NAME} variable with yours.

--- config.js
+++ config.js
@@ -2,16 +2,12 @@
  * Configuration
  */
 
-var path = require("path");
-
-// const home = require('os').homedir;
-const pwd = path.resolve(".");
-const home = path.join(pwd,"storage");
+const home = require('os').homedir;
 
 module.exports = {
 	port: 3001,
 	settingsDir : home+'/.arching-kaos-api/',
-	ipfsGateway : 'https://ipfs.arching-kaos.com/ipfs/',
+	ipfsGateway : 'http://{$IPFS_SERVER_NAME}/ipfs/',
 	esotericGateway : 'http://127.0.0.1:8080/ipfs/',
 	downloadsFolder : home+'/.arching-kaos-api/downloads/',
 	ipList: home+'/.arching-kaos-api/ipList.json',
--- configure.sh
+++ configure.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-sed -i.bak -e 's/{$HOME}/\/home\/kaotisk/' install.sh
+sed -i.bak -e 's/{$IPFS_SERVER_NAME}/ipfs.arching-kaos.local/' config.js
+sed -i.bak -e 's/{$HOME}/\/home\/kaotisk/g' install.sh
 sed -i.bak -e 's/{$HOME}/\/home\/kaotisk/' install.sh
 
 sh ./api-dirs.sh
