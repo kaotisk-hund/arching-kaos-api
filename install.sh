e0d28c0dcbfb546902d2355323c2f87bea3cb27b
kaotisk <kaotisk@arching-kaos.com>
2021-03-06 21:41:28 +0200

    Changed sequence, seperate build from start!!!

--- install.sh
+++ install.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
 docker build -t api .
-docker run --name api -d --restart=always -p 3001:3001 -v $PWD/storage/.arching-kaos-api:/root/.arching-kaos-api -v $PWD/etc/ssb-pub-data:/root/.ssb --network=host api
