f9e2b5c76278ebfc479911f55bc7e84e75faa5eb
kaotisk <kaotisk@arching-kaos.com>
2021-09-02 13:09:56 +0300

    Make use of recently uploaded image on Docker

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-podman run --pod arching-kaos --name api -d --restart=always -v $PWD/storage/.arching-kaos-api:/root/.arching-kaos-api -v $PWD/etc/ssb-pub-data:/root/.ssb api
+podman run --pod arching-kaos --name api -d --restart=always -v $PWD/storage/.arching-kaos-api:/root/.arching-kaos-api -v $PWD/etc/ssb-pub-data:/root/.ssb docker.io/kaotisk/arching-kaos-api
